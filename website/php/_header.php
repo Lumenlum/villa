906ce7a6a58a28d6ce7df08aac50e6996f4c22e5
Lumenlum <115624862+Lumenlum@users.noreply.github.com>
2023-05-24 14:28:06 +0200

    header af

--- website/php/_header.php
+++ website/php/_header.php
@@ -1,11 +1,15 @@
 
 <header>
     <nav class="navWrapper">
-        <ul class="navBar">
-            <img src="" alt="">
+        <ul class="navBar1">
+            <img src="/website/img/logo.png" alt="">
             <h2>MijnVilla</h2>
-            <li><a href="">Home</a></li>
-            <li><a href="">contact</a></li>
+            <a href="/website/php/index.php" class="NavItem">Home</a>
+            <a href="/website/php/contact.php" class="NavItem">Contact</a>
+        </ul>
+        <ul class="navBar2">
+            <a href=""><p id="login">Log in</p></a>
+            <a href=""><p id="register">Register</p></a>
         </ul>
     </nav>
 </header>
