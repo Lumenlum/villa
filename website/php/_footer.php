f1bba58526d9b158bcdfae768f377e1d263cf974
Lumenlum <115624862+Lumenlum@users.noreply.github.com>
2023-05-26 14:04:49 +0200

    responsive

--- website/php/_footer.php
+++ website/php/_footer.php
@@ -11,14 +11,15 @@
 <div class="line">
     <hr class="line_item">
 </div>
+<div class="main_foot">
+    <div class="socials">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
 
-<div class="socials">
-    <span class="dot"></span>
-    <span class="dot"></span>
-    <span class="dot"></span>
-    <span class="dot"></span>
-</div>
-
-<div class="copyright">
-    <p class="copy">© Photo, Inc. 2019. We love our users!</p>
+    <div class="copyright">
+        <p class="copy">© Photo, Inc. 2019. We love our users!</p>
+    </div>
 </div>
