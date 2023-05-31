a3724011d7362258c025c4e5391b247cbd8c5a8e
Lumenlum <115624862+Lumenlum@users.noreply.github.com>
2023-05-31 13:03:09 +0200

    slideshow

--- website/php/info.php
+++ website/php/info.php
@@ -15,12 +15,45 @@
     </header>
 
     <main>
-        <!-- Place your main content here -->
-        
+        <!-- https://www.w3schools.com/howto/howto_js_slideshow.asp -->
+        <div class="slideshow-container">
+
+
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="../img/info/oriole_way_9.jpg" style="width:100%">
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="../img/info/Roca-Llisa.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="../img/info/Villa-Indigo-12.jpg" style="width:100%">
+    <div class="text">Caption Three</div>
+  </div>
+
+  
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
     </main>
 
     <footer>
         <?php include '_footer.php' ?>
     </footer>
 </body>
+<script src="../js/info/info.js"></script>
 </html>
