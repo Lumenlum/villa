d889fc0d352ddced87a31ee9100831d703caecad
John <95253777+Maruiiss@users.noreply.github.com>
2023-06-06 14:16:31 +0200

    format van index.php fixen
    eerste deel

--- website/php/index.php
+++ website/php/index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../css/home/index.css" />
-    <link rel="stylesheet" href="./_footer.php">
-    <link rel="stylesheet" href="./_header.php">
-    <link rel="stylesheet" href="../css/menu/footer.css">
-    <link rel="stylesheet" href="../css/menu/header.css">
-    <title>Document</title>
-</head>
-<body>
+    <link rel="stylesheet" href="./_footer.php" />
+    <link rel="stylesheet" href="./_header.php" />
+    <link rel="stylesheet" href="../css/menu/footer.css" />
+    <link rel="stylesheet" href="../css/menu/header.css" />
+  </head>
+
+  <body>
     <header>
-    <?php include '_header.php' ?>
+      <?php include '_header.php' ?>
     </header>
 
-    <main>
-        <!--hier de html doen-->
-        <h1>Mijn Villa</h1>
+    <div class="back"></div>
+
+    <h1>Mijn Villa</h1>
     <p>
       Welkom bij onze exclusieve collectie van luxe villa's. <br />
       Bij ons vindt u de meest stijlvolle en comfortabele woningen <br />
@@ -27,18 +27,21 @@
       Verken nu ons assortiment en vindt de villa van uw dromen.
     </p>
     <div class="flex-container">
-      <div class="flex-item"> <img onclick="window.location.href='info.php?id=1'" class="een" src="../img/1.jpg"></div>
-      <div class="flex-item"><img onclick="window.location.href='info.php?id=2'" src="../img/2.jpg" ></div>
-      <div class="flex-item"><img onclick="window.location.href='info.php?id=3'" src="../img/3.webp" alt=""></div>
-      <div class="flex-item"> <img class="vier" src="./index/4.jpg" alt=""></div>
-      <div class="flex-item"><img class="vijf" src="./index/5.jpeg" alt=""></div>
-      <div class="flex-item"><img class="zes" src="./index/6.jpeg" alt=""></div> 
-      <div class="back"></div>
+      <div class="flex-item"><img class="een" src="../img/1.jpg" /></div>
+      <div class="flex-item"><img class="twee" src="../img/2.jpg" /></div>
+      <div class="flex-item"><img class="drie" src="../img/3.webp" alt="" />
+      </div>
+      <div class="flex-item"><img class="vier" src="../img/4.jpg" alt="" />
+      </div>
+      <div class="flex-item">
+        <img class="vijf" src="../img/5.jpeg" alt="" />
+      </div>
+      <div class="flex-item">
+        <img class="zes" src="../img/6.jpeg" alt="" />
+      </div>
     </div>
-    </main>
-
     <footer>
     <?php include '_footer.php' ?>
     </footer>
-</body>
+  </body>
 </html>
