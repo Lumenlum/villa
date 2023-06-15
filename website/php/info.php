9437b88aba0b681811c51ef89a113b93ff226c30
Lumenlum <115624862+Lumenlum@users.noreply.github.com>
2023-06-15 19:05:12 +0200

    nieuw info

--- website/php/info.php
+++ website/php/info.php
@@ -126,29 +126,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   ?>
 </div>
 
-<h1>Villa Sereno Azul</h1>
-<p>Introducing Villa Sereno Azul, a modern sanctuary nestled in the hills. With stunning views, sleek design, and luxurious amenities, this exquisite retreat offers a perfect blend of comfort and sophistication. Experience the ultimate getaway and indulge in the serenity of this captivating villa.
-</p>
-
-<h1>Villa Solara Vista</h1>
-<p>Introducing Villa Solara Vista, a contemporary masterpiece situated on the pristine shores of a breathtaking beach. Immerse yourself in the epitome of coastal luxury as this modern villa embraces the panoramic ocean views and offers a seamless fusion of indoor-outdoor living spaces. Relax and rejuvenate in this coastal haven where modern elegance meets 
-</p>
-
-<h1>Villa Serene Breeze</h1>
-<p>Villa Serene Breeze a modern retreat nestled amidst the enchanting woods. Immerse yourself in the serene beauty of nature as you indulge in the contemporary elegance of this secluded villa. Discover the perfect harmony between modern design and rustic charm, where floor-to-ceiling windows showcase the lush surroundings and a private pool invites you to relax and unwind. </p>
-
-<!DOCTYPE html>
-
-<html lang="en">
-
-
-
 <div class="google-map">
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.274257380938!2d-70.56068388481569!3d41.45496659976631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e52963ac45bbcb%3A0xf05e8d125e82af10!2sDos%20Mas!5e0!3m2!1sen!2sus!4v1671220374408!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
   </div>  
-
 
 <div class="bod">
   <h3>Bieden</h3>
